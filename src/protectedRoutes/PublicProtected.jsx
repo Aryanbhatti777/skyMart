82fcb772a5968ce7548d754933514c612db8c9b9
Aryan <bhattiaryan777@gmail.com>
2026-08-15 11:40:48 +0530

    cleaned

--- src/protectedRoutes/PublicProtected.jsx
+++ src/protectedRoutes/PublicProtected.jsx
@@ -1,12 +1,12 @@
 
 import { useSelector } from "react-redux";
 import { Navigate, Outlet} from "react-router";
-import Loader from "../shared/Loader";
+
 
 const PublicProtected = () => {
   const user = useSelector(state => state.auth.user);
   if (user) {
-    <Loader/>
+
     return <Navigate to="/main" replace/>
   }
 
